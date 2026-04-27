9f38c05ea8f74aaba6d24f2b4aec504540054c66
erkebulan777@gmail.com <erkebulan777@gmail.com>
2026-04-27 15:30:56 +0500

    Добавлен атрибут Windows и уточнены подписи кнопок
    Добавлен атрибут [SupportedOSPlatform("Windows")] для класса RibbonBuilder. Обновлены подписи кнопок экспорта стилей на панели Ribbon для лучшей читаемости и соответствия терминологии.

--- AutoBIMFusion/Application/Ribbon/RibbonBuilder.cs
+++ AutoBIMFusion/Application/Ribbon/RibbonBuilder.cs
@@ -1,10 +1,13 @@
 using Autodesk.Windows;
+using System.Runtime.Versioning;
 
 namespace AutoBIMFusion.Application.Ribbon;
 
 /// <summary>
 /// Строит вкладку AutoBIMFusion на Ribbon AutoCAD.
 /// </summary>
+///
+[SupportedOSPlatform("Windows")]
 internal static class RibbonBuilder
 {
     public static void CreateTab()
@@ -26,8 +29,8 @@ internal static class RibbonBuilder
         panelSource.Items.Add(CreateLargeButton("AutoBIMFusionBtn3", "eTransmit ZIP", "CreateETransmitZip "));
         panelSource.Items.Add(CreateLargeButton("AutoBIMFusionBtn4", "Слить стили", "MergeTextStyles "));
         panelSource.Items.Add(CreateLargeButton("AutoBIMFusionBtn5", "Соединить линии", "JOIN_LINES "));
-        panelSource.Items.Add(CreateLargeButton("AutoBIMFusionBtn6", "Экспорт TEXT стилей", "ExportTextStylesToMd "));
-        panelSource.Items.Add(CreateLargeButton("AutoBIMFusionBtn7", "Экспорт DIM стилей", "ExportDimStylesToMd "));
+        panelSource.Items.Add(CreateLargeButton("AutoBIMFusionBtn6", "Экспорт Текстовых стилей", "ExportTextStylesToMd "));
+        panelSource.Items.Add(CreateLargeButton("AutoBIMFusionBtn7", "Экспорт Размерных стилей", "ExportDimStylesToMd "));
 
         RibbonTab tab = new() { Id = "AutoBIMFusion.RibbonTab", Title = "AutoBIMFusion" };
         tab.Panels.Add(new RibbonPanel { Source = panelSource });
