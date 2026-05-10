dd569514b49a20bc060233d8e14900598f2b0d14
erkebulan777@gmail.com <erkebulan777@gmail.com>
2026-05-10 17:35:30 +0500

    auto: 2026-05-10 17:35:30

--- src/AutoBIMFusion.Merge/Combine/Layouts/DimensionStyleNormalizer.cs
+++ src/AutoBIMFusion.Merge/Combine/Layouts/DimensionStyleNormalizer.cs
@@ -40,9 +40,7 @@ internal static class DimensionStyleNormalizer
             dim.Dimtmove = 0;
             dim.Dimscale = targetVisualScale;
             dim.Dimlfac = linearScaleMultiplier;
-            ResetTextPositionForce(dim);
-
-            TouchControlPoints(dim);
+            DimensionTextAnchor.AnchorTextToMidpoint(dim);
             dim.RecomputeDimensionBlock(true);
         }
     }
@@ -69,46 +67,9 @@ internal static class DimensionStyleNormalizer
 
         dim.TextRotation = 0.0;
         dim.Dimtmove = 0;
-        ResetTextPositionForce(dim);
-
-        TouchControlPoints(dim);
+        DimensionTextAnchor.AnchorTextToMidpoint(dim);
         dim.RecomputeDimensionBlock(true);
 
         trx.Commit();
     }
-
-    // AutoCAD игнорирует true, если флаг уже был включён после клонирования.
-    private static void ResetTextPositionForce(Dimension dim)
-    {
-        dim.UsingDefaultTextPosition = false;
-        dim.UsingDefaultTextPosition = true;
-
-        Point3d textPt = dim.TextPosition;
-        if (Math.Abs(textPt.Z) != 0)
-        {
-            dim.TextPosition = new Point3d(textPt.X, textPt.Y, 0);
-        }
-    }
-
-    // Форсирует пересчёт OCS/WCS — AutoCAD кэширует старую геометрию до явного переприсваивания точек.
-    private static void TouchControlPoints(Dimension dim)
-    {
-        if (dim is RotatedDimension rotDim)
-        {
-            rotDim.XLine1Point = rotDim.XLine1Point;
-            rotDim.XLine2Point = rotDim.XLine2Point;
-            rotDim.DimLinePoint = rotDim.DimLinePoint;
-        }
-        else if (dim is AlignedDimension alDim)
-        {
-            alDim.XLine1Point = alDim.XLine1Point;
-            alDim.XLine2Point = alDim.XLine2Point;
-            alDim.DimLinePoint = alDim.DimLinePoint;
-        }
-        else
-        {
-            dim.TextPosition = dim.TextPosition;
-        }
-    }
 }
-
