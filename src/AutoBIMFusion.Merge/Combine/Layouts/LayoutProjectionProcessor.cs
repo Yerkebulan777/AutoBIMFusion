7bb8e9f54b886444a15f7349a16b0a6720529c62
erkebulan777@gmail.com <erkebulan777@gmail.com>
2026-05-10 22:47:07 +0500

    Filter layout entities by title block bounds

--- src/AutoBIMFusion.Merge/Combine/Layouts/LayoutProjectionProcessor.cs
+++ src/AutoBIMFusion.Merge/Combine/Layouts/LayoutProjectionProcessor.cs
@@ -63,7 +63,15 @@ internal static class LayoutProjectionProcessor
             return new LayoutProjectionResult(null, ViewportScaleNormalizer.WorkingScaleMultiplier, 1.0);
         }
 
-        Extents3d? paperBounds = ModelSpaceTrimmer.ComputeBounds(db, paperIds, log);
+        Extents3d? titleBlockBounds = FindTitleBlockBounds(db, layoutName);
+        using ObjectIdCollection filteredIds = FilterEntitiesByBounds(db, paperIds, titleBlockBounds);
+
+        if (filteredIds.Count == 0)
+        {
+            return new LayoutProjectionResult(null, ViewportScaleNormalizer.WorkingScaleMultiplier, 1.0);
+        }
+
+        Extents3d? paperBounds = ModelSpaceTrimmer.ComputeBounds(db, filteredIds, log);
 
         if (!paperBounds.HasValue)
         {
@@ -75,6 +83,105 @@ internal static class LayoutProjectionProcessor
 
         Extents3d? frameBounds = MovePaperToModelSpace(db, layoutName, matrix, log);
         return new LayoutProjectionResult(frameBounds, ViewportScaleNormalizer.WorkingScaleMultiplier, 1.0);
+    }
+
+    /// <summary>
+    /// Находит рамку-штамп (BlockReference с максимальной площадью) в указанном листе.
+    /// </summary>
+    private static Extents3d? FindTitleBlockBounds(Database db, string layoutName)
+    {
+        using ObjectIdCollection paperIds = LayoutUtil.GetPaperSpaceEntities(db, layoutName, excludeViewports: true);
+
+        Extents3d? bestExtents = null;
+        double bestArea = 0.0;
+
+        using Transaction tr = db.TransactionManager.StartTransaction();
+
+        foreach (ObjectId id in paperIds)
+        {
+            if (tr.GetObject(id, OpenMode.ForRead) is not BlockReference br)
+            {
+                continue;
+            }
+
+            Extents3d? ext = ExtentsUtils.TryGetExtents(br);
+            if (!ext.HasValue)
+            {
+                continue;
+            }
+
+            double width = ext.Value.MaxPoint.X - ext.Value.MinPoint.X;
+            double height = ext.Value.MaxPoint.Y - ext.Value.MinPoint.Y;
+            double area = width * height;
+
+            if (area > bestArea)
+            {
+                bestArea = area;
+                bestExtents = ext.Value;
+            }
+        }
+
+        tr.Commit();
+        return bestExtents;
+    }
+
+    /// <summary>
+    /// Фильтрует объекты листа, оставляя только те, что пересекаются с рамкой-штампом
+    /// или находятся внутри неё. Сама рамка всегда включается в результат.
+    /// </summary>
+    private static ObjectIdCollection FilterEntitiesByBounds(Database db, ObjectIdCollection paperIds, Extents3d? boundingBox)
+    {
+        if (!boundingBox.HasValue)
+        {
+            return paperIds;
+        }
+
+        ObjectIdCollection filtered = [];
+        Extents3d bounds = boundingBox.Value;
+
+        using Transaction tr = db.TransactionManager.StartTransaction();
+
+        foreach (ObjectId id in paperIds)
+        {
+            if (tr.GetObject(id, OpenMode.ForRead) is not Entity ent)
+            {
+                continue;
+            }
+
+            // Обязательно добавляем саму рамку-штамп (BlockReference с совпадающими габаритами)
+            if (ent is BlockReference)
+            {
+                Extents3d? ext = ExtentsUtils.TryGetExtents(ent);
+                if (ext.HasValue && ExtentsApproxEqual(ext.Value, bounds))
+                {
+                    _ = filtered.Add(id);
+                    continue;
+                }
+            }
+
+            Extents3d? entityExt = ExtentsUtils.TryGetExtents(ent);
+            if (entityExt.HasValue && ExtentsUtils.AabbIntersect(bounds, entityExt.Value))
+            {
+                _ = filtered.Add(id);
+                continue;
+            }
+
+            if (ExtentsUtils.IsEntityPointIn(ent, bounds))
+            {
+                _ = filtered.Add(id);
+            }
+        }
+
+        tr.Commit();
+        return filtered;
+    }
+
+    private static bool ExtentsApproxEqual(Extents3d a, Extents3d b, double tolerance = 1e-6)
+    {
+        return Abs(a.MinPoint.X - b.MinPoint.X) <= tolerance
+            && Abs(a.MinPoint.Y - b.MinPoint.Y) <= tolerance
+            && Abs(a.MaxPoint.X - b.MaxPoint.X) <= tolerance
+            && Abs(a.MaxPoint.Y - b.MaxPoint.Y) <= tolerance;
     }
 
     /// <summary>
@@ -99,8 +206,16 @@ internal static class LayoutProjectionProcessor
             return null;
         }
 
+        Extents3d? titleBlockBounds = FindTitleBlockBounds(db, layoutName);
+        using ObjectIdCollection filteredIds = FilterEntitiesByBounds(db, paperIds, titleBlockBounds);
+
+        if (filteredIds.Count == 0)
+        {
+            return null;
+        }
+
         ObjectId msId = SymbolUtilityServices.GetBlockModelSpaceId(db);
-        using ViewportTransformer.CloneTransformResult cloneResult = ViewportTransformer.DeepCloneAndTransform(db, paperIds, paperBtrId, msId, matrix, log);
+        using ViewportTransformer.CloneTransformResult cloneResult = ViewportTransformer.DeepCloneAndTransform(db, filteredIds, paperBtrId, msId, matrix, log);
 
         EraseBlockContents(db, paperBtrId);
 
@@ -126,5 +241,3 @@ internal static class LayoutProjectionProcessor
         }
     }
 }
-
-
